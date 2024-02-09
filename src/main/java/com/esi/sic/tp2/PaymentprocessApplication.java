f01cc769da5295490b460ba76b6c112c5ca385d6
ashendhia <jh_loukkaf@esi.dz>
2024-02-09 16:37:49 +0100

    TP2 version 0.1

---
+++ src/main/java/com/esi/sic/tp2/PaymentprocessApplication.java
@@ -0,0 +1,7 @@
+package com.esi.sic.tp2;
+import org.camunda.bpm.application.ProcessApplication;
+import org.camunda.bpm.application.impl.ServletProcessApplication;
+@ProcessApplication("Paymentprocess App")
+public class PaymentprocessApplication extends ServletProcessApplication {
+// empty implementation
+}
